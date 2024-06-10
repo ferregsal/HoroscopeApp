be2a4df08af81c520e5e5071027abcd093c382d6
fregidorsalazar <fregidorsalazar@gmail.com>
2024-06-10 13:41:02 +0200

    Primer Update Horoscope.kt creado

---
+++ app/src/main/java/com/example/myapplication/Horoscope.kt
@@ -0,0 +1,4 @@
+package com.example.myapplication
+
+class Horoscope (val id: String, val name: String, val logo:Int) {
+}
--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,8 +2,20 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
+    val horoscopeList:List<Horoscope> = listOf(
+        Horoscope("aries","Aries",0),
+        Horoscope("taurus","Taurus",0),
+        Horoscope("gemini","Gemini",0),
+        Horoscope("cancer","Cancer",0),
+        Horoscope("leo","Leo",0),
+        Horoscope("virgo","Virgo",0),
+        Horoscope("aries","Aries",0),
+        Horoscope("aries","Aries",0),
+        Horoscope("aries","Aries",0),
+    )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
